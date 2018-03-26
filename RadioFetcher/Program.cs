8422b091bb5e1fcd31c80bc3acaccc7b5beaf232
Henry <hesipp@gmail.com>
2018-03-26 14:32:08 -0500

    Some docs

--- RadioFetcher/Program.cs
+++ RadioFetcher/Program.cs
@@ -17,30 +17,37 @@ namespace RadioFetcher
 
 
 
-
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
 		public static void Main(string[] args)
 		{
-			
+			// Normally this would be an arg but honestly I just run this from 
+            // visual studio so... manually change this
 			var fetchOrProcess = "Process";
 
 			Task.Run(async () =>
 			{
+                // Here we process the prefetched data
+
 				if (fetchOrProcess == "Process")
 				{
 					await Processor.Process();
 				}
 				else {
+                    // This loop fetches the data from wikipedia
 					Dictionary<string, List<Dictionary<string, string>>> stationInfosByState = await Fetcher.FetchStationUrlsPerState();
 
 
 					string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 					System.IO.Directory.CreateDirectory(path + "/RadioData");
 
-
+                    // Save the data to a folder in our desktop.
+                    // Just in case we have an error later when transferring to our SQL db.
 					foreach (KeyValuePair<string, List<Dictionary<string, string>>> pair in stationInfosByState)
 					{
 						var stationinfos = pair.Value;
-						//write it incase we fuck up!
 						var woahtherebuddy = JsonConvert.SerializeObject(stationinfos, Formatting.Indented);
 						System.IO.File.WriteAllText(path + $"/RadioData/{pair.Key}.json", woahtherebuddy);
 					}
